7dee5eecd22be12ceafb9f320476360875b89d4f
direct808 <direct808@yandex.ru>
2019-09-02 22:51:16 +0300

    fix

--- src/rnd-name.ts
+++ src/rnd-name.ts
@@ -2,14 +2,12 @@ import * as namesFemale from "./names-female.json";
 import * as namesMale from "./names-male.json";
 import uniqueRandomArray = require("unique-random-array");
 
-type Gender = "male" | "female";
-
 const names = namesMale.concat(namesFemale);
 const namesRnd = uniqueRandomArray(names);
 const namesFemaleRnd = uniqueRandomArray(namesFemale);
 const namesMaleRnd = uniqueRandomArray(namesMale);
 
-export default function rndName(gender?: Gender): string {
+export default function rndName(gender?: "male" | "female"): string {
     if (gender === "male") {
         return namesMaleRnd();
     }
